8a581ec6980e4852efb6f9f137bb5b3af1c85368
Italo Carvalho <emaildoitalo@gmail.com>
2018-11-23 09:46:04 -0200

    update views/theme

--- MVCForum.Website/Themes/Metro/Content/Scripts/general.js
+++ MVCForum.Website/Themes/Metro/Content/Scripts/general.js
@@ -1,4 +1,9 @@
-﻿$(function () {
-
+﻿$(document).ready( function () {
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).prev().children('i').removeClass("fa-chevron-down").addClass("fa-chevron-up");
+    });
+    
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).prev().children('i').removeClass("fa-chevron-up").addClass("fa-chevron-down");
+    });
 });
-
